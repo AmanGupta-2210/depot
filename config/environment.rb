34d85b58769616a356b8bcbdb10ea5e57b0b879f
Aman Gupta <aman.gupta@giglrr.com>
2015-10-13 00:34:41 +0530

    Save Changes

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Depot::Application.initialize!
+Depot::Application.configure do 
+	config.action_mailer.delivery_method = :smtp
+
+	config.action_mailer.smtp_settings = {
+		address: 				"smtp.zoho.com",
+		port:     				587,
+		authentication: 		"plain",
+		user_name: 				"test@test.com",
+		password:  				"giglrr123$",
+		enable_starttls_auto:   true
+	}
+end
